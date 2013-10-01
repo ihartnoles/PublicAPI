8314b5d8b8f9da89e726c688dc4c843dc2c3c645
Isaac Hartstein <ihartstein@ubuntu.(none)>
2013-10-01 16:08:31 -0400

    first commit for public api

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyPublicAPI::Application.config.secret_token = '52253e0a1311549688e32d582ca8ce13865fd66380bf6dab125409fca2a4e56e080a1694d6c906d35451ce63f8555924d0d8348b9c45464b5d599b503273a780'
---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+products = Product.create([{ :reference => 'learning java', :quantity => 2 }, { :reference => 'kicking ass with ruby' , :quantity => 0}])
